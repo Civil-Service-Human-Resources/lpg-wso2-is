d1bbc315e11ebd07b15fb176983387080edace1b
silvanodossan <silvano@superroot.eu>
2018-02-05 16:40:45 +0000

    echo version number deployed

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 SERVICE=${1}
 
-#clone ansible repository and set application version
+#clone ansible repository
 git clone https://github.com/Civil-Service-Human-Resources/lpg-ansible-mvp.git
 cd lpg-ansible-mvp
+
+#set application version, this is the version that will be deployed
 echo "$SERVICE: $TRAVIS_COMMIT" > group_vars/all/${SERVICE} || exit 2
 
 #run ansible
@@ -12,3 +14,4 @@ sudo pip install ansible || exit 2
 echo $mvp_test | base64 -d | ./envVar.py > mvp_test && chmod 600 mvp_test
 echo $vaultpassword | base64 -d | ./envVar.py > vault.yml
 ansible-playbook site.yml -i environments/test -t ${SERVICE} || exit 2
+echo "---------- $TRAVIS_COMMIT : deployed to  test ----------"
